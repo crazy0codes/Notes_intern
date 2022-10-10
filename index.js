b495ffb0dbd1f001bb27319ac414cb4743d7df69
crazy0codes <madhanpanja@gmail.com>
2022-10-10 17:58:15 +0530

    change()

---
+++ index.js
@@ -0,0 +1,18 @@
+/*
+
+var title = [];
+var data  = [];
+document.getElementById('create').addEventListener('click',()=>{
+    let x = document.getElementById('newtitle').innerText;
+    let y = document.getElementById('newcontent').innerText;
+    let z = document.getElementById('mahaan');
+    let creator = document.createElement('div');
+    creator.setAttribute('id',x);
+    creator.setAttribute('class','newnotes');
+    let textnode = document.createTextNode(y);
+    creator.appendChild(textnode);
+    z.appendChild(creator);
+    z.style.display = "block";
+})
+
+*/
