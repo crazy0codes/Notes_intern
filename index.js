33044dac389fe686cebb6bfe6fea59b743db9a53
crazy0codes <madhanpanja@gmail.com>
2022-10-11 09:04:31 +0530

    major changes

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
-/*
-
 var title = [];
 var data  = [];
 document.getElementById('create').addEventListener('click',()=>{
-    let x = document.getElementById('newtitle').innerText;
-    let y = document.getElementById('newcontent').innerText;
-    let z = document.getElementById('mahaan');
-    let creator = document.createElement('div');
-    creator.setAttribute('id',x);
-    creator.setAttribute('class','newnotes');
-    let textnode = document.createTextNode(y);
-    creator.appendChild(textnode);
-    z.appendChild(creator);
-    z.style.display = "block";
-})
+    let x = document.getElementById('note-title').innerText; // Given title is taken as X
 
-*/
+    let y = document.getElementById('note-content').innerText; // Given Content is taken as Y
+
+    let Newnote = document.createElement('div');
+    Newnote.setAttribute('id',x);
+    Newnote.setAttribute('class','newnote'); // Created a container to wrap Title and content
+    document.getElementById('demo').appendChild(Newnote);
+
+    let Heading = document.createElement('h4')
+    Heading.setAttribute('class','heading'); //Created a div tag to put heading
+    let HeadingNode = document.createTextNode(x); 
+    Heading.appendChild(HeadingNode);
+    Newnote.appendChild(Heading,Newnote.children[0]);
+
+    let Content = document.createElement('div');
+    Content.setAttribute('class','content'); // Create a div tag to put Note content
+    let ContentNode = document.createTextNode(y); 
+    Content.appendChild(ContentNode);
+    Newnote.appendChild(Content,Newnote.children[-1]);
+
+
+})
