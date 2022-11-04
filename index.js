da518e7bd214f9b57c596db4397dc4dfd02da6ec
crazy0codes <madhanpanja@gmail.com>
2022-11-04 23:27:26 +0530

    change

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-/*var title = [];
-var data  = [];
-
+let trash = () => {
+    console.log("Madhan");
+}
+let edit = () => {
+    console.log("Masdf");
+}
+let pin = () => {
+    console.log("Maadfn");
+}
 document.getElementById('create').addEventListener('click',()=>{
     let x = document.getElementById('note-title').innerText; // Given title is taken as X
 
@@ -21,17 +27,21 @@ document.getElementById('create').addEventListener('click',()=>{
     let Trash = document.createElement('img');
     Trash.setAttribute('src','img/trash.png');
     Trash.setAttribute('id','delete');
+    Trash.addEventListener('click',trash);
     Controls.appendChild(Trash);
 
     let Edit = document.createElement('img');
     Edit.setAttribute('src','img/pencil.png');
-    Edit.setAttribute('id','edit')
+    Edit.setAttribute('id','edit');
+    Trash.addEventListener('click',edit);
     Controls.appendChild(Edit);
 
     let Pin = document.createElement('img');
     Pin.setAttribute('src','img/thumbtack.png');
     Pin.setAttribute('id','pin');
+    Trash.addEventListener('click',pin);
     Controls.appendChild(Pin);
+
 // Images are used instead of svg
     let Heading = document.createElement('h4')
     Heading.setAttribute('class','heading'); //Created a div tag to put heading
@@ -46,13 +56,3 @@ document.getElementById('create').addEventListener('click',()=>{
     Newnote.appendChild(Content);
 
 });
-
-let Notedelete = document.getElementById('delete');
-Notedelete.addEventListener('click',()=>{
-    document.getElementsByTagName('demo').remove();
-})
-*/
-
-function create() {
-    let box = document.querySelector(".container");
-};
