ecd4103144844f0962f7fa8273e455e96483c2a2
crazy0codes <madhanpanja@gmail.com>
2022-11-05 11:46:51 +0530

    Event method is added an understood successsful

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-let trash = () => {
+/*let trash  = () => {
     console.log("Madhan");
 }
-let edit = () => {
+function edit (){
     console.log("Masdf");
 }
-let pin = () => {
+function pin () {
     console.log("Maadfn");
 }
 document.getElementById('create').addEventListener('click',()=>{
@@ -27,7 +27,7 @@ document.getElementById('create').addEventListener('click',()=>{
     let Trash = document.createElement('img');
     Trash.setAttribute('src','img/trash.png');
     Trash.setAttribute('id','delete');
-    Trash.addEventListener('click',trash);
+    Trash.onclick = trash;
     Controls.appendChild(Trash);
 
     let Edit = document.createElement('img');
@@ -56,3 +56,20 @@ document.getElementById('create').addEventListener('click',()=>{
     Newnote.appendChild(Content);
 
 });
+*/
+let trash  = (event) => {
+    event.target.parentElement.parentElement.remove();
+}
+let edit = () => {
+    console.log("Masdf");
+}
+let pin = () => {
+    console.log("Maadfn");
+}
+let create = () => {
+    let note = document.createElement('div');
+    let container = document.getElementsByClassName("container")[0];
+    note.setAttribute("class","newnote");
+    note.innerHTML = `<section id = "controls"><img src = "img/trash.png" id = "delete" onclick = "trash(event)"><img src = "img/pencil.png" id = "edit" onclick = "edit(event)"><img src = "img/thumbtack.png" id = "pin" onclick = "pin(event)"></section>`
+    container.appendChild(note);
+}
