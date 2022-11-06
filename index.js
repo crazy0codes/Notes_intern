6e337c1e364a382ed72a179c115d009eb4ebb7da
crazy0codes <madhanpanja@gmail.com>
2022-11-06 09:33:31 +0530

    change

--- index.js
+++ index.js
@@ -24,24 +24,3 @@ const edit = (event) => {
 const pin = (event) => {
     document.querySelector('#pinned').appendChild(event.target.parentElement.parentElement)
 }
-/*const trash  = (event) => {
-    event.target.parentElement.parentElement.remove();
-}
-const edit = (event) => {
-    let edit = event.target.parentElement.parentElement;
-    console.log(edit);
-}
-const pin = (event) => {
-    console.log(event);
-}
-let create = () => {
-    let note = document.createElement('div');
-    let container = document.getElementById("demo");
-    note.setAttribute("class","newnote");
-    note.innerHTML = `
-    <section id = "controls"><img src = "img/trash.png" id = "delete" onclick = "trash(event)"><img src = "img/pencil.png" id = "edit" onclick = "edit(event)"><img src = "img/thumbtack.png" id = "pin" onclick = "pin(event)"></section>
-    <h4 class = "heading">${x}</h4>
-    <div class = "content">${y}</div>
-    `
-    container.appendChild(note);
-}*/
